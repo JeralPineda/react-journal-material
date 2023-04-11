169adfcb2fb5625e3c98e7e662278d0a2092782b
Jeral Pineda <jpineda@banhcafe.hn>
2023-04-11 09:25:59 -0600

    Configurando las rutas de la pagina

--- src/JournalApp.jsx
+++ src/JournalApp.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import { AppRouter } from './router/AppRouter';
 
 export const JournalApp = () => {
   return (
-    <div>
-      <h1>JournalApp</h1>
-    </div>
+    <>
+      <AppRouter />
+    </>
   );
 };
