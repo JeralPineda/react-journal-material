0664ca37c55051c1b6619f83cbc005eb3926282f
Jeral Pineda <jpineda@banhcafe.hn>
2023-04-12 10:25:59 -0600

    Mantener el estado de la autenticacion al recargar

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,15 +1,42 @@
-import { Route, Routes } from 'react-router-dom';
+import { onAuthStateChanged } from 'firebase/auth';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Navigate, Route, Routes } from 'react-router-dom';
+
 import { AuthRoutes } from '../auth/routes/AuthRoutes';
+import { FirebaseAuth } from '../firebase/config';
 import { JournalRoutes } from '../journal/routes/JournalRoutes';
+import { login, logout } from '../store/auth';
+import { CheckingAuth } from '../ui';
 
 export const AppRouter = () => {
+  const { status } = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    onAuthStateChanged(FirebaseAuth, async (user) => {
+      const { displayName, uid, email, photoURL } = user;
+      if (!user) return dispatch(logout());
+
+      dispatch(login({ displayName, uid, email, photoURL }));
+    });
+  }, []);
+
+  if (status === 'checking') {
+    return <CheckingAuth />;
+  }
+
   return (
     <Routes>
+      {status === 'authenticated' ? <Route path="/*" element={<JournalRoutes />} /> : <Route path="/auth/*" element={<AuthRoutes />} />}
+
+      <Route path="/*" element={<Navigate to="/auth/login" />} />
+
       {/* Login y Registro */}
-      <Route path="/auth/*" element={<AuthRoutes />} />
+      {/* <Route path="/auth/*" element={<AuthRoutes />} /> */}
 
       {/* JournalApp */}
-      <Route path="/*" element={<JournalRoutes />} />
+      {/* <Route path="/*" element={<JournalRoutes />} /> */}
       <Route />
     </Routes>
   );
