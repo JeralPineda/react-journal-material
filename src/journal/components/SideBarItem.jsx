d8a63a76962d143ede0a3b968f2d8120777ca4d4
Jeral Pineda <jpineda@banhcafe.hn>
2023-04-13 14:07:48 -0600

    Activar una nota

--- src/journal/components/SideBarItem.jsx
+++ src/journal/components/SideBarItem.jsx
@@ -1,15 +1,23 @@
 import { TurnedInNot } from '@mui/icons-material';
 import { Grid, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material';
 import { useMemo } from 'react';
+import { useDispatch } from 'react-redux';
+import { setActiveNote } from '../../store/journal';
 
-export const SideBarItem = ({ title, body }) => {
+export const SideBarItem = ({ title = '', body, id, date, imagesUrl = [] }) => {
+  const dispatch = useDispatch();
+
   const newTitle = useMemo(() => {
     return title.length > 17 ? title.substring(0, 17) + '...' : title;
   }, [title]);
 
+  const onSelectNote = () => {
+    dispatch(setActiveNote({ title, body, id, date }));
+  };
+
   return (
     <ListItem disablePadding>
-      <ListItemButton>
+      <ListItemButton onClick={onSelectNote}>
         <ListItemIcon>
           <TurnedInNot />
         </ListItemIcon>
