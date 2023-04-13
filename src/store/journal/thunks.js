6ea8b2fe112681b4fc67cc8e17637894b4447530
Jeral Pineda <jpineda@banhcafe.hn>
2023-04-13 13:17:16 -0600

    Cargar notas de firestore

--- src/store/journal/thunks.js
+++ src/store/journal/thunks.js
@@ -1,6 +1,7 @@
 import { collection, doc, setDoc } from 'firebase/firestore/lite';
 import { FirebaseDB } from '../../firebase/config';
-import { addNewEmptyNote, savingNewNote, setActiveNote } from './journalSlice';
+import { loadNotes } from '../../helpers';
+import { addNewEmptyNote, savingNewNote, setActiveNote, setNotes } from './journalSlice';
 
 export const startNewNote = () => {
   return async (dispatch, getState) => {
@@ -20,5 +21,14 @@ export const startNewNote = () => {
 
     dispatch(addNewEmptyNote(newNote));
     dispatch(setActiveNote(newNote));
+  };
+};
+
+export const startLoadingNotes = () => {
+  return async (dispatch, getState) => {
+    const { uid } = getState().auth; //uid for user
+
+    const notes = await loadNotes(uid);
+    dispatch(setNotes(notes));
   };
 };
