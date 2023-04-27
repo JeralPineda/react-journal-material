6f850bc0db574d542216bc938d7b1f0e7b7e1367
Jeral Pineda <jpineda@banhcafe.hn>
2023-04-27 07:31:09 -0600

    test: Agregando prueba para eliminar img de cloudinary

--- tests/helpers/fileUpload.test.js
+++ tests/helpers/fileUpload.test.js
@@ -1,4 +1,12 @@
+import { v2 as cloudinary } from 'cloudinary';
 import { fileUpload } from '../../src/helpers/fileUpload';
+
+cloudinary.config({
+  cloud_name: 'dyszc49bz',
+  api_key: '755234936665418',
+  api_secret: 'zwUEtPEnXDSn_c46cDbrJiV6CkY',
+  secure: true,
+});
 
 describe('Pruebas en fileUpload', () => {
   test('debe de subir el archivo correctamente a cloudinary', async () => {
@@ -9,5 +17,19 @@ describe('Pruebas en fileUpload', () => {
 
     const url = await fileUpload(file);
     expect(typeof url).toBe('string');
+
+    //Limpieza
+    const segments = url.split('/');
+    const imageId = segments[segments.length - 1].replace('.jpg', '');
+    await cloudinary.api.delete_resources(['journal' + imageId]); //Eliminando imagen
+  });
+
+  test('debe de retornar null', async () => {
+    // const blob = await resp.blob();
+    const file = new File([], 'image.jpg');
+
+    const url = await fileUpload(file);
+    // console.log(url);
+    expect(url).toBe(null);
   });
 });
