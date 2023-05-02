c2d3de41d3a4aa88427852cd300847ad9e5a8084
Jeral Pineda <jpineda@banhcafe.hn>
2023-05-02 16:10:36 -0600

    test: Prueba completa sobre insersion y eliminacion de notas

--- tests/store/journal/thunks.test.js
+++ tests/store/journal/thunks.test.js
@@ -1,4 +1,11 @@
-import { startNewNote } from '../../../src/store/journal/thunks';
+import { collection, deleteDoc, getDocs } from 'firebase/firestore/lite';
+import { FirebaseDB } from '../../../src/firebase/config';
+import {
+  addNewEmptyNote,
+  savingNewNote,
+  setActiveNote,
+} from '../../../src/store/journal/journalSlice';
+import { startNewNote, startSaveNote } from '../../../src/store/journal/thunks';
 
 describe('Pruebas en Journal Thunks', () => {
   const dispatch = jest.fn();
@@ -10,5 +17,32 @@ describe('Pruebas en Journal Thunks', () => {
     const uid = 'TEST-UID';
     getState.mockReturnValue({ auth: { uid: uid } });
     await startNewNote()(dispatch, getState);
+
+    expect(dispatch).toHaveBeenCalledWith(savingNewNote());
+    expect(dispatch).toHaveBeenCalledWith(
+      addNewEmptyNote({
+        body: '',
+        title: '',
+        id: expect.any(String),
+        date: expect.any(Number),
+        imageUrls: [],
+      })
+    );
+    expect(dispatch).toHaveBeenCalledWith(
+      setActiveNote({
+        body: '',
+        title: '',
+        id: expect.any(String),
+        date: expect.any(Number),
+        imageUrls: [],
+      })
+    );
+
+    //Borrar notas de firebase
+    const collectionRef = collection(FirebaseDB, `${uid}/journal/notes`);
+    const docs = await getDocs(collectionRef);
+    const deletePromises = [];
+    docs.forEach((doc) => deletePromises.push(deleteDoc(doc.ref)));
+    await Promise.all(deletePromises);
   });
 });
