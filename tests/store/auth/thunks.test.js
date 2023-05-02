028e5345bfcb550b5342d01952197bb30a66eecf
Jeral Pineda <jpineda@banhcafe.hn>
2023-05-02 09:54:00 -0600

    test(thunks): Pruebas con startLoginWithEmailPassword

--- tests/store/auth/thunks.test.js
+++ tests/store/auth/thunks.test.js
@@ -1,9 +1,16 @@
-import { signInWithGoogle } from '../../../src/firebase/providers';
+import {
+  loginWithEmailPassword,
+  logoutFirebase,
+  signInWithGoogle,
+} from '../../../src/firebase/providers';
 import { checkingCredentials, login, logout } from '../../../src/store/auth';
 import {
   checkingAuthentication,
   startGoogleSignIn,
+  startLoginWithEmailPassword,
+  startLogout,
 } from '../../../src/store/auth/thunks';
+import { clearNotesLogout } from '../../../src/store/journal';
 import { demoUser } from '../../fixtures/authFixtures';
 
 jest.mock('../../../src/firebase/providers');
@@ -38,5 +45,25 @@ describe('Pruebas en AuthThunks', () => {
 
     expect(dispatch).toHaveBeenCalledWith(checkingCredentials());
     expect(dispatch).toHaveBeenCalledWith(logout(loginData));
+  });
+
+  test('startLoginWithEmailPassword debe de llamar checkingCredentials y login - Exitoso', async () => {
+    const loginData = { ok: true, ...demoUser };
+    const formData = { email: demoUser.email, password: '123456' };
+
+    await loginWithEmailPassword.mockResolvedValue(loginData);
+
+    await startLoginWithEmailPassword(formData)(dispatch);
+
+    expect(dispatch).toHaveBeenCalledWith(checkingCredentials());
+    expect(dispatch).toHaveBeenCalledWith(login(loginData));
+  });
+
+  test('startLogout debe de llamar logoutFirebase, clearNotes y logout', async () => {
+    await startLogout()(dispatch);
+
+    expect(logoutFirebase).toHaveBeenCalled();
+    expect(dispatch).toHaveBeenCalledWith(clearNotesLogout());
+    expect(dispatch).toHaveBeenCalledWith(logout());
   });
 });
